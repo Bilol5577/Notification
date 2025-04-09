1d2585d9c80dc8efb242d84816ab9f3fc2e0e589
Bilol5577 <bilolbektursunov079@gmail.com>
2025-04-09 19:22:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+Toastify({
+    text: "Well done!",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    close: true, 
+    gravity: "top", 
+    position: "right", 
+    style: {
+      background: " #3c8c79",
+    },
+    onClick: function(){} 
+  }).showToast();
+
+  document.getElementById("btn1").addEventListener("click", function () {
+    Toastify({
+      text: "Well done!",
+      duration: 3000,
+      destination: "https://github.com/apvarun/toastify-js",
+      close: true,
+      gravity: "top",
+      position: "right",
+      style: {
+        background: " #3c8c79",
+      },
+      onClick: function () {}
+    }).showToast();
+  });
+  
+// 
+
+  Toastify({
+    text: "Oh snap! Change a few things up.",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    close: true,
+    gravity: "top", 
+    position: "right", 
+    style: {
+      background: "#e74c3c",
+    },
+    onClick: function(){} 
+  }).showToast();
+
+  document.getElementById("btn2").addEventListener("click", function () {
+    Toastify({
+      text: "Oh snap! Change a few things up.",
+      duration: 3000,
+      destination: "https://github.com/apvarun/toastify-js",
+      close: true,
+      gravity: "top",
+      position: "right",
+      style: {
+        background: "#e74c3c",
+      },
+      onClick: function () {}
+    }).showToast();
+  });
+  
+// 
+
+  Toastify({
+    text: "Warning!",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    close: true,
+    gravity: "top", 
+    position: "right", 
+    style: {
+      background: "#f39c12",
+    },
+    onClick: function(){} 
+  }).showToast();
+
+  document.getElementById("btn3").addEventListener("click", function () {
+    Toastify({
+      text: "Warning!",
+      duration: 3000,
+      destination: "https://github.com/apvarun/toastify-js",
+      close: true,
+      gravity: "top",
+      position: "right",
+      style: {
+        background: "#f39c12",
+      },
+      onClick: function () {}
+    }).showToast();
+  });
+  
